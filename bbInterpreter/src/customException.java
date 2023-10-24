64b719e8a134710a9e4cba0c8df4819d1158ddd8
PascalJ13 <147179850+PascalJ13@users.noreply.github.com>
2023-10-24 09:46:50 +0100

    Delete bbInterpreter directory

--- bbInterpreter/src/customException.java
+++
@@ -1,7 +0,0 @@
-public class customException extends  Exception
-{
-    public customException(String exceptionMessage)
-    {
-        super(exceptionMessage + "\nPlease check your code to ensure all statements are delimited with a ;");
-    }
-}
